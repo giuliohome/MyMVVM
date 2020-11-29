23325eebbc848bd9e33df86818890b4420f84270
giuliohome <giuliohome@gmail.com>
2020-11-29 13:02:04 +0100

    package published

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MyMVVM
 Open source for my viewmodel implementation, to be published as a nuget package
+
+More info on my [blog](https://giuliohome.wordpress.com/2019/01/19/functional-viewmodel/)
+
+Package on [nuget](https://www.nuget.org/packages/GiulioMVVM/)
---
+++ ViewModelBase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace GiulioMVVM
+{
+ 
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Constructor
+
+        protected ViewModelBase()
+        {
+        }
+ 
+        #endregion // Constructor
+ 
+        #region DisplayName
+ 
+ 
+        public virtual string DisplayName { get; protected set; }
+ 
+        #endregion // DisplayName
+ 
+        #region Debugging Aides
+ 
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            // Verify that the property name matches a real,
+            // public, instance property on this object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                string msg = "Invalid property name: " + propertyName;
+
+                if (this.ThrowOnInvalidPropertyName)
+                    throw new Exception(msg);
+                else
+                    Debug.Fail(msg);
+            }
+        }
+ 
+        protected virtual bool ThrowOnInvalidPropertyName { get; private set; }
+ 
+        #endregion // Debugging Aides
+ 
+        #region INotifyPropertyChanged Members
+
+ 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged<X, T>(Expression<Func<X, T>> selectorExpression)
+        {
+            if (selectorExpression == null)
+                throw new ArgumentNullException("selectorExpression");
+            MemberExpression body = selectorExpression.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("The body must be a member expression");
+            OnPropertyChanged(body.Member.Name);
+        }
+
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> selectorExpression)
+        {
+            if (selectorExpression == null)
+                throw new ArgumentNullException("selectorExpression");
+            MemberExpression body = selectorExpression.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("The body must be a member expression");
+            OnPropertyChanged(body.Member.Name);
+        }
+ 
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.VerifyPropertyName(propertyName);
+
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+ 
+        #endregion // INotifyPropertyChanged Members
+ 
+    }
+
+}
