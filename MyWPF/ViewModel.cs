3c2b25ab60fceec0c00463f1b7a6239d03b7dc97
giuliohome <giuliohome@gmail.com>
2020-11-29 13:44:07 +0100

    usage example

---
+++ MyWPF/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GiulioMVVM;
+
+
+namespace MyWPF
+{
+    class ViewModel : ViewModelBase
+    {
+        private string testResult;
+
+        public string TestResult
+        {
+            get { return testResult; }
+            set {
+                testResult = value;
+                OnPropertyChanged(() => TestResult);
+            }
+        }
+
+        public ViewModel()
+        {
+            TestResult = "Test OK!";
+        }
+
+        DelegateCommand clickCommand;
+        public ICommand ClickCommand
+        {
+            get
+            {
+                if (clickCommand is null)
+                {
+                    clickCommand = new DelegateCommand(_ => runClick());
+                }
+                return clickCommand;
+            }
+        }
+
+        int times = 0;
+        private void runClick()
+        {
+
+            TestResult = $"clicked {++times} times";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Package on [nuget](https://www.nuget.org/packages/GiulioMVVM/)
 Open Source on [github](https://github.com/giuliohome/MyMVVM)
 [![Open Source Love](https://badges.frapsoft.com/os/v3/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
 ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+
+Usage example: see [MyWPF folder](MyWPF) 
